a029f061b52afe24e3f55c9e5de547b20df778d2
tanvir1017 <developer.tanvirhossain@gmail.com>
2024-04-08 15:44:10 +0600

    feat: 🎸 pushing from the pyCharm to github

---
+++ code01.py
@@ -0,0 +1,13 @@
+import turtle
+
+# Python Turtule graphic
+window = turtle.Screen()
+drawer = turtle.Turtle()
+for i in range(5):
+    drawer.forward(100)
+    drawer.right(144)
+
+
+
+# check hello world
+print("hello world")
